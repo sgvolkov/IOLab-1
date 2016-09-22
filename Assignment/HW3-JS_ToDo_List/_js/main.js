e69ebe6d9aceb0cc11aa1a07a4a5f1b5b4b03f08
‘sgvolkov’ <svolkov@ischool.berkeley.edu>
2016-09-21 18:34:40 -0700

    HW3

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(
     $("#new-item").on('click', function() {
         // once the document loads, create new item with this function
+        var user_input = $('#todo-item-input').val();
+        $('#list_todo').prepend("<li><button> Move me! </button>" + user_input + "</li>");
     })
 );
 
+//$("#new-list").on('click', function() {
+    // once the document loads, create new item with this function
+    //$('.ul_list').append($('<ol>',{
+        //text: $('#input-new-list').val()
+    //}));
+//});
+
+
 $("#list_todo").on('click', "button", function() {
         // move from list_todo container to list_completed container
+        ($(this).html("Add To Complete List"));
+        var workingItem = $(this).parent();
+        $("#working_todo").prepend(workingItem).css("color", "#B10DC9"); //changing the color of a completed item
+});
+
+
+$("#working_todo").on('click', "button", function() {
+        // move from list_todo container to list_completed container
+        ($(this).html("Add To In Progress List"));
+        var completedItem = $(this).parent();
+        $("#list_completed").prepend(completedItem).css("color", "#0074D9"); //changing the color of a completed item
 });
 
 $("#list_completed").on('click', "button", function() {
         // move back from list_completed container to list_todo container
+        ($(this).html("Add To In Progress List"));
+        var incompleteItem = $(this).parent();
+        $("#working_todo").prepend(incompleteItem);
 });
